fca69dc2615c283007670832c6c87e8ce8f01e05
Darma Yasa <darmayasa@192.168.204.108>
2022-05-23 19:35:36 +0800

    added testing helper database and create migrate database

---
+++ tests/AlbumsTableTestHelper.js
@@ -0,0 +1,40 @@
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+/* istanbul igonore file */
+const AlbumsTableTestHelper = {
+  async addAlbum({
+    id = 'album-0001',
+    name = 'album',
+    year = 2000,
+  }) {
+    const query = {
+      text: `INSERT INTO albums
+      VALUES($1, $2, $3)`,
+      values: [id, name, year],
+    };
+    const { rows } = await pool.query(query);
+    return rows;
+  },
+
+  async findAlbum(id) {
+    const query = {
+      text: `SELECT * FROM albums
+      WHERE id = $1`,
+      values: [id],
+    };
+    const { rows } = await pool.query(query);
+    return rows;
+  },
+
+  async findAlbums() {
+    const query = 'SELECT * FROM albums';
+    const { rows } = await pool.query(query);
+    return rows;
+  },
+
+  async cleanTable() {
+    await pool.query('TRUNCATE table albums, songs');
+  },
+};
+
+module.exports = AlbumsTableTestHelper;
